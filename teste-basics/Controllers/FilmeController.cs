a221b3ae12f20afd1e57c39234990bf4139d9d59
Vinícius Floriano <viniciushpfloriano@outlook.com>
2022-10-24 20:38:39 -0300

    Padronizando GET e POST para os padrões REST

--- teste-basics/Controllers/FilmeController.cs
+++ teste-basics/Controllers/FilmeController.cs
@@ -11,26 +11,32 @@ namespace Controllers
         private static List<Filme> filmes = new List<Filme>();
         private static int id= 1;
 
-        [HttpPost] // HttpPost indica que é um método Post de acordo com o padrão REST
-        public void AdicionarFilme([FromBody] Filme filme) // FromBody indica que o parâmetro será passado no body da requisição
+        [HttpPost] // HttpPost indica que é um método Post de acordo com o padrão REST.
+        public IActionResult AdicionarFilme([FromBody] Filme filme) // FromBody indica que o parâmetro será passado no body da requisição.
         {
             filme.Id = id++;
             filmes.Add(filme);
+            return CreatedAtAction(nameof(ListarFilmePorId), new {ID = filme.Id} ,filme); // gera código 201 Created exibindo no HEADER Location qual rota utilizar para consumir a informação criada.
         }
 
         
-        [HttpGet] // HttpGet indica que é um método Get de acordo com o padrão REST
+        [HttpGet] // HttpGet indica que é um método Get de acordo com o padrão REST.
         /*public List<Filme> ListarFilme()*/
-        public IEnumerable<Filme> ListarFilme() // Alterado para garantir a integridade do código para qualquer classe que implemente esta interface
+        public IActionResult ListarFilme() // Alterado para garantir a integridade do código para qualquer classe que implemente esta interface.
         {
-            return filmes;
+            return Ok(filmes);
         }
 
         [HttpGet("{id}")] // HttpGet com um parâmetro de ID que deve ser passado na URL.
         /*public List<Filme> ListarFilme()*/
-        public Filme RecuperaFilmesPorId(int id)
+        public IActionResult ListarFilmePorId(int id)
         {
-            return filmes.FirstOrDefault(filme => filme.Id == id);
+            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id); // Retorna o ID procurado ou nulo como retorno padrão.
+            if(filme != null) // Trata os retornos nulos.
+            {
+                return Ok(filme); // retorna o filme e gera status 200 ok.
+            }
+            return NotFound(); // retorna null e gera status 404 NotFound.
         }
     }
 }
