70b15ddb0ab8e55b05f50869e66bce1ddc2aa908
Vinícius Floriano <90642803+VHPFchefe@users.noreply.github.com>
2022-10-24 19:40:06 -0300

    Initial commit

---
+++ teste-basics/Controllers/FilmeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        
+        
+        [HttpPost] // HttpPost indica que é um método Post de acordo com o padrão REST
+        public void AdicionarFilme([FromBody] Filme filme) // FromBody indica que o parâmetro será passado no body da requisição
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+
+        /*
+        [HttpGet]
+        public void VerFilme()
+        {
+            
+            Console.WriteLine("Estou vendo um filme!");
+        }
+        */
+
+    }
+}
---
+++ teste-basics/Models/Filme.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class Filme
+    {
+        // Definindo o Título como campo obrigatório no body
+        [Required(ErrorMessage ="A requisição deve conter o campo Título!")]
+        public string Titulo { get; set; }
+
+        public string Diretor { get; set; }
+
+        [StringLength(30,ErrorMessage = "O Gênero não deve conter mais que 30 caracteres!")]
+        [Required(ErrorMessage = "A requisição deve conter o campo Gênero!")]
+        public string Genero { get; set; }
+        
+        // Definindo a Duração como campo com um range de 1 a 600 inteiros no body
+        [Range(1,600, ErrorMessage = "O campo Duração deve conter de 1 a 600 minutos!")]
+        public int Duracao { get; set; }
+
+    }
+}
