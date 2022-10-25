e4c80641572dea90fa765041be09a9d28b3c66a5
Vinícius Floriano <viniciushpfloriano@outlook.com>
2022-10-24 22:54:37 -0300

    Preparando o ambiente para a primeira migration

--- teste-basics/Program.cs
+++ teste-basics/Program.cs
@@ -1,6 +1,16 @@
+using Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// no .Net 6.0 o Startup e Program foram mesclados! 
+builder.Services.AddDbContext<FilmeContext>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration.GetConnectionString("FilmeConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
