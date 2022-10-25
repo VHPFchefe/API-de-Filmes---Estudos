dba865dc31abb6fce2695ff46ef3e3cd6a590f6f
Vinícius Floriano <viniciushpfloriano@outlook.com>
2022-10-24 23:51:30 -0300

    Informações persistentes no banco
    Criar e Consultar OK (50% do CRUD)
    Próximos passos é editar e remover

--- teste-basics/Controllers/FilmeController.cs
+++ teste-basics/Controllers/FilmeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Models;
 
@@ -8,30 +9,34 @@ namespace Controllers
     [ApiController]
     public class FilmeController : ControllerBase
     {
-        private static List<Filme> filmes = new List<Filme>();
-        private static int id= 1;
+        private FilmeContext _context;
+
+        public FilmeController(FilmeContext context)
+        {
+            _context = context;
+        }
 
         [HttpPost] // HttpPost indica que é um método Post de acordo com o padrão REST.
         public IActionResult AdicionarFilme([FromBody] Filme filme) // FromBody indica que o parâmetro será passado no body da requisição.
         {
-            filme.Id = id++;
-            filmes.Add(filme);
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
             return CreatedAtAction(nameof(ListarFilmePorId), new {ID = filme.Id} ,filme); // gera código 201 Created exibindo no HEADER Location qual rota utilizar para consumir a informação criada.
         }
 
         
         [HttpGet] // HttpGet indica que é um método Get de acordo com o padrão REST.
         /*public List<Filme> ListarFilme()*/
-        public IActionResult ListarFilme() // Alterado para garantir a integridade do código para qualquer classe que implemente esta interface.
+        public IEnumerable<Filme> ListarFilme() // Alterado para garantir a integridade do código para qualquer classe que implemente esta interface.
         {
-            return Ok(filmes);
+            return _context.Filmes;
         }
 
         [HttpGet("{id}")] // HttpGet com um parâmetro de ID que deve ser passado na URL.
         /*public List<Filme> ListarFilme()*/
         public IActionResult ListarFilmePorId(int id)
         {
-            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id); // Retorna o ID procurado ou nulo como retorno padrão.
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id); // Retorna o ID procurado ou nulo como retorno padrão.
             if(filme != null) // Trata os retornos nulos.
             {
                 return Ok(filme); // retorna o filme e gera status 200 ok.
